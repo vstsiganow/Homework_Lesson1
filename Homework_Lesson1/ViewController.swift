0b09cbad1965f37c1c5d810b589ddca0aa4eb5a4
vstsiganow <slavok-48@mail.ru>
2021-05-19 10:04:35 +0300

    Minor edits settings

--- Homework_Lesson1/ViewController.swift
+++ Homework_Lesson1/ViewController.swift
@@ -10,10 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    @IBOutlet weak var topUIView: UIView!
-    
-    @IBOutlet weak var botUIView: UIView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
