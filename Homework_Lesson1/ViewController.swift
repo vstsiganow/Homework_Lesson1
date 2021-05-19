d207de19797fd1c624ccd5432996a8c9bc330c4d
vstsiganow <slavok-48@mail.ru>
2021-05-19 09:53:47 +0300

    Initial Commit

---
+++ Homework_Lesson1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Homework_Lesson1
+//
+//  Created by vtsyganov on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var topUIView: UIView!
+    
+    @IBOutlet weak var botUIView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+    
+    
+}
+
